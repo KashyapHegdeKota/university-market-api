96f20dd349543d1b03ffc3f30e79875edba5847e
KashyapHegdeKota <kota2022kashyap@gmail.com>
2026-02-10 22:44:04 -0700

    Add has_many listings to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  # This line is missing!
+  has_many :listings, dependent: :destroy
+  
+  validates :username, presence: true
+  validates :email, presence: true
 end
