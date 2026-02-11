648e6d0db5012341cb140ddcfac3712ad3c6e4f1
KashyapHegdeKota <kota2022kashyap@gmail.com>
2026-02-10 22:29:37 -0700

    Added show route to handle a single listing request.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # This line tells Rails to create all standard routes (index, create, etc.) for listings
-  resources :listings, only: [:index, :create]
+  resources :listings, only: [:index, :create, :show]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   get "up" => "rails/health#show", as: :rails_health_check
