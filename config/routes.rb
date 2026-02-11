03779040806ddea5ecb23bac5103fb086e77d13e
KashyapHegdeKota <kota2022kashyap@gmail.com>
2026-02-10 19:16:24 -0700

    Added listing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get "listings/index"
-  get "listings/create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # This line tells Rails to create all standard routes (index, create, etc.) for listings
+  resources :listings, only: [:index, :create]
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  get "up" => "rails/health#show", as: :rails_health_check
 end
